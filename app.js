141f84b96fac2b1b6f0890adfd025c9fc6f11581
V Anand Suhaas <79833145+suhaas02@users.noreply.github.com>
2022-07-20 13:00:37 +0530

    Corrected Port

--- app.js
+++ app.js
@@ -81,4 +81,4 @@ router.post('/', async (req, res, next) => {
     res.render('index', { error: err.message })
   })
 
-app.listen(3000,() => console.log("Listening on port 3000"));
+app.listen(process.env.PORT || 3000,() => console.log("Listening on port 3000"));
